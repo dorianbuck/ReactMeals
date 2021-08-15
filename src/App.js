1eb424f22125204a3e4f6b6d22ebc09d2ac47043
Dorian Buck <[dorianbuck@gmail.com]>
2021-08-15 22:01:54 +0200

    added more functionality to Cart overlay

--- src/App.js
+++ src/App.js
@@ -8,13 +8,17 @@ function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
-    
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false)
   }
 
   return (
     <React.Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
