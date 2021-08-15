e22c38cf9fe162bac017c8a4793ee6187860e099
Dorian Buck <[dorianbuck@gmail.com]>
2021-08-15 21:44:23 +0200

    added a modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    
+  }
+
   return (
     <React.Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
