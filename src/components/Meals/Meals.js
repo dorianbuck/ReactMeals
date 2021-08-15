0d67f07d41208813dcf011e4f0ab7bab44debeb5
Dorian Buck <[dorianbuck@gmail.com]>
2021-08-15 20:35:58 +0200

    first commit

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import MealsSummery from "./MealsSummery";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+  return (
+    <React.Fragment>
+      <MealsSummery />
+      <AvailableMeals />
+    </React.Fragment>
+  );
+};
+
+export default Meals;
